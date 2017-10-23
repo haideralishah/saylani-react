5e42ad3cec176a521fd1901a0c7f38fa22274bef
Haider <headeralishah@gmail.com>
2017-10-23 11:39:06 +0500

    Class code of chapter 1 and 2

---
+++ Chapter 1 - 3/js/app.js
@@ -0,0 +1,138 @@
+
+
+// ReactDOM.render(
+//     React.createElement('h1', {title: 'hello world'}, 'Hello World'),
+//     document.getElementById('container')
+// );
+
+
+
+
+
+
+
+
+
+
+
+// var helloworld = React.createElement('a', { title: 'hello world', href: 'https://www.google.com', target: '_blank' }, 'Abu Ubaida Bin Jarrah RZ');
+
+
+// ReactDOM.render(
+//     helloworld,
+//     document.getElementById('container')
+// );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var h1 = React.createElement('h1', null, 'Hello React World');
+// var h2 = React.createElement('h2', null, 'Hello React World');
+
+// ReactDOM.render(
+//     React.createElement('div', null, h1, h2)
+//     ,
+// document.getElementById('container')
+// );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class MyName extends React.Component {
+//     render() {
+//         return React.createElement('h1', null, this.props.customeName);
+//     }
+// }
+
+// var mughal = React.createElement(MyName, { customeName: 'Mughal' });
+// var haider = React.createElement(MyName, { customeName: 'haider' });
+
+// ReactDOM.render(
+//     React.createElement('div', null, mughal, haider),
+//     document.getElementById('container')
+// );
+
+// let h1 = React.createElement('h1', null, 'Hello world!')
+// class HelloWorld extends React.Component {
+//     render() {
+//         return React.createElement('div', null, h1, h1)
+//     }
+// }
+// ReactDOM.render(
+//     React.createElement(HelloWorld, null),
+//     document.getElementById('container')
+// )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class CustomeLink extends React.Component {
+    render() {
+        return React.createElement('a', { href: this.props.link }, this.props.linkText);
+    }
+}
+
+var google = React.createElement(CustomeLink, { link: 'https://www.google.com', linkText: 'google' });
+var facebook = React.createElement(CustomeLink, { link: 'https://www.facebook.com', linkText: 'facebook' });
+var br = React.createElement('br');
+ReactDOM.render(
+    React.createElement('div', null, google, br, facebook),
+    document.getElementById('container')
+);
